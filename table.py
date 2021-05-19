49fef4ea602068858170ec0cf97c6ff9ecc0a878
Bhavjot Grewal <bhavjot.grewal@mail.utoronto.ca>
2021-05-19 15:16:49 -0400

    Adjusted table.

--- table.py
+++ table.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import gpu
 import consoles
 import pyttsx3
+import newegg
 
 engine = pyttsx3.init()
 engine.setProperty('rate', 190)
@@ -18,30 +19,22 @@ table.add_column("Product")
 table.add_column("Available?", justify="right")
 table.add_column("Last Checked", justify="right")
 
+list_3060_newegg = newegg.load_data('newegg_products.txt')
+
 with Live(table, refresh_per_second=1, vertical_overflow="visible"):
     while True:
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        if gpu.in_stock_tuf_3080_updated():
-            table.add_row("[yellow]Newegg[/yellow]", "RTX 3080 TUF OC", "[green]in stock[green]",
-                          dt_string)
-            engine.say("30 80 New Egg")
-            engine.runAndWait()
-        else:
-            table.add_row("[yellow]Newegg[/yellow]", "RTX 3080 TUF OC", "[red]out of stock[/red]",
-                          dt_string)
-            engine.say("30 80 No where")
-            engine.runAndWait()
 
-        if consoles.in_stock_ps5_disc_updated():
-            now = datetime.now()
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            table.add_row("[blue]Best Buy[/blue]", "PS5 Disc", "[blue]in stock[blue]",
-                          dt_string)
-            engine.say("PS5 Disc Best Buy")
-            engine.runAndWait()
-        else:
-            table.add_row("[blue]Best Buy[/blue]", "PS5 Disc", "[red]out of stock[red]",
-                          dt_string)
-            engine.say("PS5 no where")
-            engine.runAndWait()
+        list_newegg = newegg.in_stock_list(list_3060_newegg)
+
+        for product in list_newegg:
+            try:
+                if product[1] == False:
+                    table.add_row("[yellow]Newegg[/yellow]", product[0], "[red]out of stock[/red]",
+                                  dt_string)
+                else:
+                    table.add_row("[yellow]Newegg[/yellow]", product[0], "[green]in stock[/green]",
+                                  dt_string)
+            except:
+                continue
