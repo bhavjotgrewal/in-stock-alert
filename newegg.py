9fb578d8e1b988df67fa50c40d582e4f876972d7
Bhavjot Grewal <bhavjot.grewal@mail.utoronto.ca>
2021-05-20 09:25:12 -0400

    Removed unnecessary variable.

--- newegg.py
+++ newegg.py
@@ -18,8 +18,6 @@ def in_stock_checker(link: str) -> List[Union[str, bool]]:
 
     try:
 
-        r = requests.get(link)
-
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
                           'like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
