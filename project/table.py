822dd744a717fb20434f38e68ce40840af75066b
Bhavjot Grewal <bhavjot.grewal@mail.utoronto.ca>
2021-05-22 20:17:44 -0400

    Refactored files

---
+++ project/table.py
@@ -0,0 +1,52 @@
+from rich.console import Console
+from rich.live import Live
+from rich.table import Table
+from datetime import datetime
+import pyttsx3
+from project import memoryexpress, newegg
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 190)
+
+console = Console()
+
+table = Table(show_header=True, header_style="bold magenta")
+
+table.add_column("Store", width=12)
+table.add_column("Product")
+table.add_column("Available?", justify="right")
+table.add_column("Last Checked", justify="right")
+
+list_newegg = newegg.load_data('data/newegg_products.txt')
+list_m_e = memoryexpress.load_data('data/memory_express_products.txt')
+
+with Live(table, refresh_per_second=2, vertical_overflow="visible"):
+    while True:
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+        # list_newegg = newegg.in_stock_list(list_newegg)
+        list_m_e = memoryexpress.in_stock_list(list_m_e)
+
+        # for product in list_newegg:
+        #     try:
+        #         if product[1] is False:
+        #             table.add_row("[yellow]Newegg[/yellow]", product[0], "[red]out of stock[/red]",
+        #                           dt_string)
+        #         else:
+        #             table.add_row("[yellow]Newegg[/yellow]", product[0], "[green]in stock[/green]",
+        #                           dt_string)
+        #     except:
+        #         continue
+
+        for product in list_m_e:
+            try:
+                if product[1] is False:
+                    table.add_row("[green]Memory Express[/green]", product[0], "[red]out of "
+                                                                               "stock[/red]",
+                                  dt_string)
+                else:
+                    table.add_row("[yellow]Newegg[/yellow]", product[0], "[green]in stock[/green]",
+                                  dt_string)
+            except:
+                continue
