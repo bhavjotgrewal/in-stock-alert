b95e8dc5eeae85dc00f0badafd273f290238c8df
Bhavjot Grewal <bhavjot.grewal@mail.utoronto.ca>
2021-05-22 20:27:19 -0400

    Removed redundant functions

--- project/memoryexpress.py
+++ project/memoryexpress.py
@@ -1,14 +1,6 @@
 from typing import List, Union
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
-
-
-def load_data(file: str) -> List[str]:
-    """Generates a list of links from a .txt file."""
-    with open(file) as f:
-        content = [line.rstrip() for line in f]
-    return content
 
 
 def in_stock_checker(link: str) -> List[Union[str, bool, int]]:
@@ -50,11 +42,3 @@ def in_stock_checker(link: str) -> List[Union[str, bool, int]]:
     except:
         print('connection blocked')
         pass
-
-
-def in_stock_list(links: list) -> List[List[Union[str, bool]]]:
-    """Returns whether products are in stock from a list of links of products."""
-    output = []
-    for link in links:
-        output.append(in_stock_checker(link))
-    return output
